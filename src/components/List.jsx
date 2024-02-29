81b06a68bfabb44488f63919d685b10357916c28
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-02-29 19:21:42 +0530

    drop indicator page.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -194,15 +194,7 @@ const Card = ({ title, id, column, handleDragStart }) => {
   );
 };
 
-const DropIndicator = ({ beforeId, column }) => {
-  return (
-    <div
-      data-before={beforeId || "-1"}
-      data-column={column}
-      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
-    />
-  );
-};
+
 
 
 
